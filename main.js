189386c99c36461482b8b483f8adf26fa019b36d
Abdallahdhouib <abdallah.dhouib@outlook.fr>
2020-12-06 20:34:26 +0100

    BetterMotherFuckingWebsite, avec une popup

---
+++ main.js
@@ -0,0 +1,49 @@
+
+// Get the modal
+let modal = document.getElementById("myModal");
+let r = document.querySelector(':root');
+// Get the button that opens the modal
+let btn = document.getElementById("oui");
+let myBtn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+
+
+// When the user clicks on the button, open the modal
+window.onload = function() {
+  modal.style.display = "block";
+}
+setTimeout( function() {
+  modal.style.display = "none";
+},10000)
+
+onclick.btn= function(){
+ modal.style.display="none";
+
+window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+
+
+}
+
+function psychedelyque() {
+  
+  r.style.setProperty('--pColor', 'blue');
+  r.style.setProperty('--pFont', 'courier');
+  r.style.setProperty('--hColor', 'green');
+  r.style.setProperty('--hFont', 'Comic Sans');
+  r.style.setProperty('--backgroundColor', 'magenta');
+
+
+}
+
+function normal(){
+
+  r.style.setProperty('--pColor', 'black');
+  r.style.setProperty('--backgroundColor',' #eeeeee');
+  r.style.setProperty('--hColor', 'black');
+  r.style.setProperty('--pFont', 'montserrat');
+  r.style.setProperty('--hFont', 'arial');
+}
+
+
+
